8f8ad765105b1ac0aff115f5fc3067de3ba691d4
lakesmoochies <tleejewno@gmail.com>
2024-12-12 23:20:21 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(".slider-one").not(".slick-initialized").slick({
+  prevArrow: ".site-slider.prev",
+  nextArrow: ".site-slider.next",
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplaySpeed: 3000,
+});
